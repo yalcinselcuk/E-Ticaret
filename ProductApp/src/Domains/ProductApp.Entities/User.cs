c57b01ba521dbe667e8d0a2da59d83e850f291a1
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-06-07 23:36:06 +0300

    user katmanları oluşturuldu

--- ProductApp/src/Domains/ProductApp.Entities/User.cs
+++ ProductApp/src/Domains/ProductApp.Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProductApp.Entities
 {
-    public class User
+    public class User : IEntity
     {
         public int Id{ get; set; }
         public string Name { get; set; }
---
+++ ProductApp/src/Infrastructure/ProductApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using ProductApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public User? Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<User?> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User?>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
